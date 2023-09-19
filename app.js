7ebc541501719b0ae3f0380b0614e5fc2f5fda3e
ehsanhasan <mahmudulhasanehsan@gmail.com>
2023-09-19 21:59:09 +0600

    js first time comit

---
+++ app.js
@@ -0,0 +1,14 @@
+var num1 = 5000;
+var num2 = 1400;
+var num3 = 900;
+
+if (num1 > num2 && num1 > num3) {
+    console.log('than number 1 e boro');
+}
+else if (num2 > num3) {
+    console.log('number 2 e boro');
+}
+
+else {
+    console.log('number 3 e boro');
+}
---
+++ variable.js
@@ -0,0 +1,36 @@
+// var num1 = 20;
+// var num2 = '10.3467785';
+// var parsInt = parseFloat(num2)
+
+// var sum = num1 + parsInt;
+// console.log(sum.toFixed(2));
+
+
+// console.log(typeof num1);
+
+var name = 'hablu';
+
+// if (name == 'hablu') {
+//     console.log('this is a hablu');
+// }
+
+// else if (name == 'meton') {
+//     console.log('this is meton');
+// }
+// else {
+//     console.log('this nothing to do anything');
+// }
+
+
+var num1 = 20;
+var num2 = 30;
+if (num1 == 20 || num2 == 100) {
+    console.log('this right the way');
+}
+
+else if (num1 == 20 && num2 == 31) {
+    console.log('ta hole aita dekaw');
+}
+else {
+    console.log('tha na hole rr kichu korar nai');
+}
